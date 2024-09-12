d2449da8646870bb47101789c27f36d03ec795c5
axrordev <foraxror@gmail.com>
2024-09-12 17:33:55 +0500

    ok

--- client/src/Components/MyCollectionPage.jsx
+++ client/src/Components/MyCollectionPage.jsx
@@ -254,35 +254,36 @@ const MyCollection = () => {
 				</div>
 			)}
 			<div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-  {collections.map((collection) => (
-    <div
-      key={collection.id}
-      className="flex flex-col bg-white border shadow-sm rounded-xl h-full"
-    >
-      <img
-        className="w-full h-48 object-cover rounded-t-xl"
-        // collection.image.filePath dan foydalanamiz
-        src={
-          collection.image
-            ? `https://axidel-ezhzgse9eyacc6e9.eastasia-01.azurewebsites.net/${collection.image.filePath}`
-            : "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80"
-        }
-        alt={collection.name}
-      />
-      <div className="p-4 md:p-5 flex-grow">
-        <h3 className="text-lg font-bold text-gray-800">{collection.name}</h3>
-        <p className="mt-1 text-gray-500">{collection.description}</p>
-        <a
-          className="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-          href="/"
-        >
-          Go somewhere
-        </a>
-      </div>
-    </div>
-  ))}
-</div>
-
+				{collections.map((collection) => (
+					<div
+						key={collection.id}
+						className="flex flex-col bg-white border shadow-sm rounded-xl h-full"
+					>
+						<img
+							className="w-full h-48 object-cover rounded-t-xl"
+							// collection.image.filePath dan foydalanamiz
+							src={
+								collection.image
+									? `https://axidel-ezhzgse9eyacc6e9.eastasia-01.azurewebsites.net/${collection.image.filePath}`
+									: "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80"
+							}
+							alt={collection.name}
+						/>
+						<div className="p-4 md:p-5 flex-grow">
+							<h3 className="text-lg font-bold text-gray-800">
+								{collection.name}
+							</h3>
+							<p className="mt-1 text-gray-500">{collection.description}</p>
+							<a
+								className="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+								href="/"
+							>
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
