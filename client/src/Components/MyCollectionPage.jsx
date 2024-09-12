074e8845087e2971553a0d492812c3312eb146c6
axrordev <foraxror@gmail.com>
2024-09-12 15:29:45 +0500

    ok

--- client/src/Components/MyCollectionPage.jsx
+++ client/src/Components/MyCollectionPage.jsx
@@ -127,7 +127,7 @@ const MyCollection = () => {
         <div>
             <button
                 onClick={openModal}
-                className="m-4 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="mx-4 mt-4 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 type="button"
             >
                 Create Collection
