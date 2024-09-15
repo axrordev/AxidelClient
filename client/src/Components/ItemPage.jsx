50e35681e553031a0284c2dbc980f5d3ffd4a51b
axrordev <foraxror@gmail.com>
2024-09-15 11:41:24 +0500

    created ItemPage

--- client/src/Components/ItemPage.jsx
+++ client/src/Components/ItemPage.jsx
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+
 
 const ItemPage = () => {
 	const [items, setItems] = useState([]);
 	const [itemName, setItemName] = useState("");
 	const { collectionId } = useParams(); // URL dan collectionId ni olamiz
+
+	// Tokenni dekodlash va userId olish funksiyasi
+	const getUserIdFromToken = () => {
+		const token = localStorage.getItem("token"); // Local storage'dan token olish
+		if (!token) {
+			console.error("No token found");
+			return null;
+		}
+
+		try {
+			const decodedToken = jwtDecode(token); // Tokenni decode qilish
+			const userId = decodedToken.Id; // 'Id' ni olish
+			return userId;
+		} catch (error) {
+			console.error("Error decoding token:", error);
+			return null;
+		}
+	};
 
 	// Sahifa yuklanganda itemlarni olish
 	useEffect(() => {
@@ -19,6 +39,7 @@ const ItemPage = () => {
 			} catch (error) {
 				console.error("Error fetching items:", error);
 			}
+			
 		};
 
 		fetchItems();
@@ -28,13 +49,20 @@ const ItemPage = () => {
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 	
+		const userId = getUserIdFromToken(); // UserId token orqali olish
+		if (!userId) {
+			console.error("No user ID found");
+			return;
+		}
+	
 		const newItem = {
 			name: itemName,
 			collectionId: parseInt(collectionId, 10),
+			userId: userId // Yangi itemga userId qo'shish
 		};
 	
 		try {
-			const token = localStorage.getItem("authToken"); // Auth tokenni localStorage yoki contextdan oling
+			const token = localStorage.getItem("token"); // Tokenni olish
 	
 			const response = await axios.post(
 				"https://axidel-ezhzgse9eyacc6e9.eastasia-01.azurewebsites.net/api/Items",
