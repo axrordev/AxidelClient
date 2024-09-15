c30e2eed74ef1d6839a2a2d835d8a271ff2ad38a
axrordev <foraxror@gmail.com>
2024-09-16 00:38:08 +0500

    test

--- client/src/Components/ItemPage.jsx
+++ client/src/Components/ItemPage.jsx
@@ -100,7 +100,7 @@ const ItemPage = () => {
 				<button type="submit">Create Item</button>
 			</form>
 
-			<div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+			<div className="p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
 				{items.map((item) => (
 					<div
 						key={item.id}
