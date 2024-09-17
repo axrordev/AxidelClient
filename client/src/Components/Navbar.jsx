745f1145be69c15e9428c47d71907314f2bb6286
axrordev <foraxror@gmail.com>
2024-09-17 15:39:33 +0500

    test

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -131,57 +131,6 @@ const Navbar = () => {
 											/>
 											<div
 												id="userDropdown"
-												className={`z-10 ${
-													isDropdownOpen ? "block" : "hidden"
-												} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
-											>
-												<div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-													<div>Bonnie Green</div>
-													<div className="font-medium truncate">
-														name@flowbite.com
-													</div>
-												</div>
-												<ul
-													className="py-2 text-sm text-gray-700 dark:text-gray-200"
-													aria-labelledby="avatarButton"
-												>
-													<li>
-														<a
-															href="/"
-															className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-														>
-															Dashboard
-														</a>
-													</li>
-													<li>
-														<a
-															href="/"
-															className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-														>
-															Settings
-														</a>
-													</li>
-													<li>
-														<a
-															href="/"
-															className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-														>
-															Earnings
-														</a>
-													</li>
-												</ul>
-												<div className="py-1">
-													<a
-														href="/"
-														onClick={handleLogout}
-														className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-													>
-														Sign out
-													</a>
-												</div>
-											</div>
-											<div
-												id="userDropdown"
 												className={`absolute right-0 mt-2 z-50 ${
 													isDropdownOpen ? "block" : "hidden"
 												} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
